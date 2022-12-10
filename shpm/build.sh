9269b790a9b00450c15c44b91d22163b7bdd0ca4
TheMadPunter <106832853+familycomicsstudios@users.noreply.github.com>
2022-12-10 14:31:19 +0000

    Created v0.5.0 update

--- shpm/build.sh
+++ shpm/build.sh
@@ -4,3 +4,6 @@ python3 -m build
 cp ../.config/pip/pip.conf.2 ../.config/pip/pip.conf
 cd dist
 pip install shpm_themadpunter-*\.whl --force-reinstall
+cd ../..
+cp shpm/shpm.sh venv/bin/shpm
+chmod +x venv/bin/shpm
